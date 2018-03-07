67d6f310f3cb438e00fa1c8b11a07b03927b5965
Louise Foussat <louisefoussat@gmail.com>
2018-03-07 11:51:43 +0100

    Navbar added with a header-style.css sheet and a js folder.

---
+++ js/kodak-script.js
@@ -0,0 +1,11 @@
+/* Navigation menu */
+
+function openNav() {
+    document.getElementById("kodakNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("kodakNav").style.width = "0%";
+}
+
+/* End of navigation menu */
