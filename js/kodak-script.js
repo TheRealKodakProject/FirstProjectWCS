6fba6613925955618f4cf4b72f2f6e992578d25d
Daniel D <daniel.deutsch25@gmail.com>
2018-03-15 10:20:25 +0100

    Polish and fix part 1

--- js/kodak-script.js
+++ js/kodak-script.js
@@ -1,11 +1,8 @@
 /* Navigation menu */
-
 function openNav() {
     document.getElementById("kodakNav").style.width = "100%";
 }
-
 function closeNav() {
     document.getElementById("kodakNav").style.width = "0%";
 }
-
 /* End of navigation menu */
